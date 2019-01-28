8ff5b8a1c8a0f5d513796b152797afea440cc894
Victor Pupim <victor.pupim@nytimes.com>
2019-01-28 03:24:55 -0500

    documentation comments

--- echo.go
+++ echo.go
@@ -4,6 +4,8 @@ import (
 	"github.com/pupimvictor/do-echo-svr/models"
 )
 
+//Echo receives a Message and return its content in a Echo object
+//if the message is nil, it will return a empty echo
 func Echo (in *models.Message)  (*models.Echo){
 	if in.Msg != nil {
 		return &models.Echo{Echo: *in.Msg}
